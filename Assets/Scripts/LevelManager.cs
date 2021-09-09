f82162588457ddf47a8777c165c70737d7c4f7d1
Leramy <borodakol@gmail.com>
2021-09-09 23:22:05 +0300

    Added LoadScrene

--- Assets/Scripts/LevelManager.cs
+++ Assets/Scripts/LevelManager.cs
@@ -2,11 +2,14 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class LevelManager : MonoBehaviour, IGameManager
 {
     public ManagerStatus status { get; private set; }
 
+    public GameObject LoadingScreen;
+    public Slider bar;
     public int curLevel { get; private set; }
     public int maxLevel { get; private set; }
 
@@ -27,14 +30,15 @@ public class LevelManager : MonoBehaviour, IGameManager
             curLevel++;
             string name = "Level " + curLevel;
             Debug.Log("Loading " + name);
-            SceneManager.LoadScene(name);
+            LoadNewScene(name);
         }
         else
         {
             Debug.Log("Last Level!");
             curLevel = 0;
             PlayerPrefs.SetInt("curLevel", curLevel);
-            SceneManager.LoadScene("Final");
+            //SceneManager.LoadScene("Final");
+            LoadNewScene("Final");
         }
     }
 
@@ -49,13 +53,15 @@ public class LevelManager : MonoBehaviour, IGameManager
     {
         string name = "Level " + curLevel;
         Debug.Log("Loading " + name);
-        SceneManager.LoadScene(name);
+        // SceneManager.LoadScene(name);
+        LoadNewScene(name);
     }
 
     public void LoadMenu()
     {
         Debug.Log("Menu");
-        SceneManager.LoadScene("Menu");
+        //SceneManager.LoadScene("Menu");
+        LoadNewScene("Menu");
     }
 
     public void StartNewGame()
@@ -64,11 +70,34 @@ public class LevelManager : MonoBehaviour, IGameManager
         curLevel = 1;
         PlayerPrefs.SetInt("curLevel", curLevel-1);
         PlayerPrefs.Save();
-        SceneManager.LoadScene("Level 1");
+        //SceneManager.LoadScene("Level 1");
+        LoadNewScene("Level 1");
     }
     public void QuiteGame()
     {
         Debug.Log("GameOver");
         Application.Quit();
+    }
+
+    public void LoadNewScene(string name)
+    {
+        LoadingScreen.SetActive(true);
+      //  SceneManager.LoadScene(name);
+        StartCoroutine(LoadAsync(name));
+
+        
+    }
+
+    IEnumerator LoadAsync(string name)
+    {
+        Debug.Log("Coroutine start");
+        AsyncOperation asyncLoad = SceneManager.LoadSceneAsync(name);
+
+        while(!asyncLoad.isDone)
+        {
+            bar.value = asyncLoad.progress;
+            yield return null;
+        }
+        LoadingScreen.SetActive(false);
     }
 }
